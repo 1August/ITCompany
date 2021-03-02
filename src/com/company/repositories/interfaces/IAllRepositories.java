e72ef6c30102162597a402543891de3216b58512
1august <Giant_Tuzik>
2021-03-03 04:20:19 +0600

    My OOP assignment 5 - company

--- src/com/company/repositories/interfaces/IAllRepositories.java
+++ src/com/company/repositories/interfaces/IAllRepositories.java
@@ -9,11 +9,9 @@ import java.util.List;
 public interface IAllRepositories {
     List<Front> getAllFront();
     List<Back> getAllBack();
-
     boolean addFront(Front front);
     boolean addBack(Back back);
     boolean deleteFront(int id) throws PSQLException;
     boolean deleteBack(int id) throws PSQLException;
-
     int getSalary();
 }
